3ce73e43780a4673fe5a79f5ca023873767e50ab
Leoncio <leoncioperez512@gmail.com>
2016-06-20 22:39:06 -0400

    changes

--- On The Map/LoginViewController.swift
+++ On The Map/LoginViewController.swift
@@ -12,11 +12,9 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    //@IBOutlet weak var facebookLoginButton: FBSDKLoginButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //FacebookClient.logout()
     }
     
     @IBAction func loginButtonClicked(sender: AnyObject) {
